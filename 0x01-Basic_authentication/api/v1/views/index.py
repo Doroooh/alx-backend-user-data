a5020fbe80f87ded831994012a9ac638f3144b67
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-13 00:06:10 +0300

    Basic authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
+""" Index view module to handle general API routes.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -8,8 +8,8 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
-    Return:
-      - the status of the API
+    Returns:
+      - JSON response with the current API status.
     """
     return jsonify({"status": "OK"})
 
@@ -17,8 +17,8 @@ def status() -> str:
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    Returns:
+      - JSON object containing the total count of each data model.
     """
     from models.user import User
     stats = {}
@@ -28,19 +28,19 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
 def unauthorized_endpoint() -> None:
-    """Endpoint that raises a 401 error.
+    """Simulated endpoint that triggers a 401 Unauthorized error.
 
     Returns:
-        None: None.
+        None: Triggers an HTTP 401 error response.
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False, methods=['GET'])
 def forbidden_endpoint() -> None:
-    """Endpoint that raises a 403 error.
+    """Simulated endpoint that triggers a 403 Forbidden error.
 
     Returns:
-        None: None.
+        None: Triggers an HTTP 403 error response.
     """
     abort(403)
